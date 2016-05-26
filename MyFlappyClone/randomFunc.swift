8c72a4444a9ac80dfcfaebfde1762fb993ff7de1
Entgoddess <paige.walters92@gmail.com>
2016-05-26 17:06:01 -0500

    second commit

---
+++ MyFlappyClone/randomFunc.swift
@@ -0,0 +1,25 @@
+//
+//  randomFunc.swift
+//  MyFlappyClone
+//
+//  Created by Paige Walters on 5/25/16.
+//  Copyright © 2016 Paige Walters. All rights reserved.
+//
+
+import Foundation
+
+import CoreGraphics
+
+public extension CGFloat {
+    
+    
+    public static func random() -> CGFloat {
+        
+        return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
+    }
+    
+    public static func random(min min : CGFloat, max : CGFloat) -> CGFloat{
+        
+        return CGFloat.random() * (max - min) + min
+    }
+}
